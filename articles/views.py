b6afcd1bd2e4e79438c63c37d0d984ae14ff0900
minju <kimminju3335@naver.com>
2023-11-02 14:54:05 +0900

    Delete 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
@@ -21,4 +21,29 @@ def detail(request, id):
 
     return render(request, 'detail.html', context)
 
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
+
+    # {% url 'articles:index' %}
+    # return redirect('articles:index')
+    return redirect('articles:detail', id=article.id)
+
+
+
+def delete(request, id):
+    article = Article.objects.get(id=id)
+    article.delete()
+
+    return redirect('articles:index')
     
